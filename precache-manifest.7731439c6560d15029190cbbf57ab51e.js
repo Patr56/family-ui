a383f13e96751f48997692d21e49fcf2f97e0629
Patr56 <Patr56@users.noreply.github.com>
2020-02-23 14:22:16 +0000

    deploy: 305c4328b71fe5060ba879fed605bf537dd788d8

---
+++ precache-manifest.7731439c6560d15029190cbbf57ab51e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57f323d8b1adb31e9d6f0ff9b6e9710f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f73d6bdd73d0ae371c6e",
+    "url": "/static/css/main.b5bac8fb.chunk.css"
+  },
+  {
+    "revision": "23d3a47e97b132b42362",
+    "url": "/static/js/2.ec71b70f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.ec71b70f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f73d6bdd73d0ae371c6e",
+    "url": "/static/js/main.e5820ccd.chunk.js"
+  },
+  {
+    "revision": "3d61cddc85d871dcb246",
+    "url": "/static/js/runtime-main.2b96b210.js"
+  },
+  {
+    "revision": "9571ceae61063b81aa078db58854cb19",
+    "url": "/static/media/at-solid.9571ceae.svg"
+  },
+  {
+    "revision": "a4595829064776b68de23235d7ea8f71",
+    "url": "/static/media/bg.a4595829.svg"
+  },
+  {
+    "revision": "cd3cb58ae446233aa9ec17286af6b9fd",
+    "url": "/static/media/book-open-solid.cd3cb58a.svg"
+  },
+  {
+    "revision": "c073570774b99c9ec6b9257ac72c8ef1",
+    "url": "/static/media/calendar-alt-solid.c0735707.svg"
+  },
+  {
+    "revision": "6873bb5f5c776cbb1ef1cd3b6c782feb",
+    "url": "/static/media/images-solid.6873bb5f.svg"
+  },
+  {
+    "revision": "67b896c3e1121b172b19890bd221c6d6",
+    "url": "/static/media/map-solid.67b896c3.svg"
+  },
+  {
+    "revision": "0c6a5d8d9e2636ae5104bf03d3b294bd",
+    "url": "/static/media/mobile-alt-solid.0c6a5d8d.svg"
+  }
+]);
